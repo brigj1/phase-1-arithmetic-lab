d0d2a84e957e5d2b94f05c0dc6648c874764182b
Brigham <brig.johnson@gmail.com>
2022-04-27 14:26:01 -0400

    commit tree dd11ba361fc64f01d95b177a7891f10772cbe5a6

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 20;
+const num4 = 16;
 
-// Write your code here
+/*
+const n1 = 2;
+const n2 = 31;
+
+function multiply() {
+    return num1 * num2;
+}
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+const multiply = (n1, n2) => n1 * n2;
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+*/
+//const multiply = () => num1 * num2;
+const multiply = num1 * num2;
+//console.log(multiply(num1, num2));
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = num3 % num4;
+
+const max = Math.max(1,2,3,4,5,20);
